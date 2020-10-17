a8fbada11283bd2f8cd1ddd788a9249153324fa2
TheStorybot <hornerdylan@gmail.com>
2020-10-17 14:02:27 -0700

    utilized alert, write, var, method

---
+++ project1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+var x ="Hello, World!" /// defining a string variable///
+document.write(x);    ///directing document to print the variable///
+
+var y ="This is how we do iiit!"
+document.write(y);
+
+
+var z ="This- is- how- we- do- it."
+window.alert(z);
+
+document.write("T\'was a long time ago.")
+
+var L ="Hello!"
++"Hello to you sir!" ///concatinating string variable for L ///
++"And you as well!"
+
+document.write(L);
+
+var Dog = "The good boy", Cat ="The small angry dog", Lizard ="Licks\'"  ///naming and defining multiple variables///
+
+document.write(Cat)
+
+var angry = "Very mad"
+var angry = angry.fontcolor("red");
+
+document.write(angry);
+
+var Dog = Dog.fontcolor("orange");  ///assigning a font color for a string variable
+var Cat = Cat.fontcolor("red");
+var Lizard = Lizard.fontcolor("green");
+
+document.write(Cat, Dog, Lizard);
+
+document.write(5 + 4) /// completing a method ///
